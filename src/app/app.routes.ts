519944b004bba2c45a2e59a8d80075114e6c1a16
cain. <caintheterrible@gmail.com>
2025-05-07 04:34:54 +0000

    UPDATED: configured dynamic loading of routes from the features/authentication module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'',
+    loadChildren:()=>
+      import('./features/authentication/authentication.module').then(
+        (m)=>m.AuthenticationModule
+      ),
+  },
+  // To add other routes here
+];
